1e488e2f0f782b3061d530f3b6d6e31a596e4286
rnard <rnardestani@gmail.com>
2025-07-13 00:15:25 +0330

    update

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+import os
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from model.repository.database_creator import create_database
+from view.user_view import UserView
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+create_database()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+ui = UserView()
