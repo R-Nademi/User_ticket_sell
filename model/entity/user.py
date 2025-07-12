64602d7d5f5b7d99803956eb16253893a265a3ee
rnard <rnardestani@gmail.com>
2025-07-12 23:59:44 +0330

    update

---
+++ model/entity/user.py
@@ -0,0 +1,78 @@
+from model.tools.validation import *
+
+
+
+
+
+class User:
+    def init(self, code, name, family, username,password,role,locked=False):
+        self.code = code
+        self.name = name
+        self.family = family
+        self.username = username
+        self.password = password
+        self.role = role
+        self.locked = locked
+
+
+
+    def __repr__(self):
+        return f"{self.__dict__}"
+
+
+
+
+
+    @property
+    def code(self):
+        return self._code
+
+    @code.setter
+    def code(self, value):
+        code_validation(value)
+        self._code = value
+
+    @property
+    def name(self):
+        return  self._name
+
+    @name.setter
+    def name(self, value):
+        name_validation(value)
+        self._name = value
+
+    @property
+    def family(self):
+        return  self._family
+
+    @family.setter
+    def family(self, value):
+        family_validation(value)
+        self._family = value
+
+    @property
+    def username(self):
+        return  self._username
+
+    @username.setter
+    def username(self, value):
+        username_validation(value)
+        self._username = value
+
+    @property
+    def password(self):
+        return  self._password
+
+    @password.setter
+    def password(self, value):
+        password_validation(value)
+        self._password = value
+
+    @property
+    def role(self):
+        return  self._role
+
+    @role.setter
+    def role(self, value):
+        role_validation(value)
+        self._role = value
