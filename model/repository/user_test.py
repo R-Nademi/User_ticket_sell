acb6a070039501fc26667c2f396ecd5c29b19be5
rnard <rnardestani@gmail.com>
2025-07-14 20:00:35 +0330

    update

---
+++ model/repository/user_test.py
@@ -0,0 +1,14 @@
+from model.entity.ticket import Ticket
+
+
+ticket = Ticket(1, "Ali", "AliPour",
+                   "1360", "Mashhad",
+           "Tehran", "10.30", "12.00",
+                "Airline","A21","30$")
+
+
+print(ticket)
+
+
+def __repr__(self):
+    return f"{self.__dict__}"
---
+++ web_main.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template,request
+
+from controller.user_controller import UserController
+
+app = Flask(__name__, template_folder="view")
+
+@app.route("/")
+def index():
+    return render_template("user.html")
+
+@app.route("/users", methods=["POST"])
+def save_user():
+    user_controller = UserController()
+    status, message = user_controller.save(
+        request.form["name"],
+        request.form["family"],
+        request.form["username"],
+        request.form["password"],
+        request.form["role"],
+        False
+    )
+    print(status, message)
+    return render_template("user.html")
+
+
+
+app.run(host="192.168.39.100", port=80, debug=True)
