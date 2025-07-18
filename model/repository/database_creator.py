fe6a2bf68cc9896e11e374a0d82465f8478b73c2
rnard <rnardestani@gmail.com>
2025-07-18 17:44:33 +0330

    update

---
+++ model/repository/database_creator.py
@@ -0,0 +1,38 @@
+import sqlite3
+
+def create_database():
+    # اتصال
+    connection = sqlite3.connect("tickets_db.sqlite")
+
+    # ساخت جدول
+    # عملیات ذخیره، ویرایش، حذف و انواع جستجو و گزارش
+    cursor = connection.cursor()
+
+    cursor.execute("""
+                   (CREATE TABLE IF NOT EXISTS USERS,
+                   
+                       code     integer primary key AUTOINCREMENT ,
+                       name     text not null,
+                       family   text not null,
+                       username text not null unique,
+                       password text not null,
+                       role     text not null,
+                       locked   tinyint default 0
+                   )
+                   """)
+
+    cursor.execute("""
+                   (CREATE TABLE IF NOT EXISTS Patient
+                   
+                       code         integer primary key,
+                       name         text not null,
+                       family       text not null,
+                       phone_number text
+                   )
+                   """)
+
+    connection.commit()
+
+    # قطع اتصال
+    cursor.close()
+    connection.close()
