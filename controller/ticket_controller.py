90253121161385c5f03c81af677c2fb81da844a9
rnard <rnardestani@gmail.com>
2025-07-30 23:24:31 +0330

    update

--- controller/ticket_controller.py
+++ controller/ticket_controller.py
@@ -53,31 +53,31 @@ class TicketController:
         except Exception as e:
             return False, f"Error find all tickets {e}"
 
-    def find_by_ticket(self, code):
+    def find_by_code(self, code):
         try:
             ticket_repo = TicketRepository()
             return True, ticket_repo.find_by_ticket(code)
         except Exception as e:
             return False, f"Error find ticket code : {code} Error :{e}"
 
-    def find_by_ticket(self, name, family):
+    def find_by_name_family(self, name, family):
         try:
             ticket_repo = TicketRepository()
             return True, ticket_repo.find_by_ticket(name, family)
         except Exception as e:
             return False, f"Error find tickets name : {name} - family {family} -- Error {e}"
 
-    def find_by_ticket(self, name):
+    def find_by_name(self, name):
         try:
             ticket_repo = TicketRepository()
             return True, ticket_repo.find_by_ticket(name)
         except Exception as e:
             return False, f"Error find tickets name : {name}  -- Error {e}"
 
-    def find_by_ticket(self, name, password):
+    def find_by_password(self, name, password):
         try:
             ticket_repo = TicketRepository()
-            return True, ticket_repo.find_by_ticket(name,password)
+            return True, ticket_repo.find_by_password(name,password)
         except Exception as e:
             return False, f"Error find tickets  name:password : {name}:{password}  -- Error {e}"
 
